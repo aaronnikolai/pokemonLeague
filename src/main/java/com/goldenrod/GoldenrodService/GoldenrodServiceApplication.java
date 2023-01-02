7eac1eeb800c20a7c3fdbb32b1492f7bfdf2dc08
aaronnikolai <aaronnikolai2015@gmail.com>
2023-01-02 15:46:28 -0800

    Maven project with spring boot

---
+++ src/main/java/com/goldenrod/GoldenrodService/GoldenrodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goldenrod.GoldenrodService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenrodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenrodServiceApplication.class, args);
+	}
+
+}
